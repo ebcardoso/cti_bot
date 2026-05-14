f72ce27c9e8f5c39ba09cf5c8c9e1010cebed5d6
Breno Cardoso <elisiobreno@gmail.com>
2026-05-14 16:02:58 -0300

    0.1.0 - Creating method that send tickets to telegram

---
+++ date_utils.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+# Converte a data no formato do SUAP em um datetime
+def date_parser(date_str):
+    months = {
+        "Janeiro": "01",
+        "Fevereiro": "02",
+        "Março": "03",
+        "Abril": "04",
+        "Maio": "05",
+        "Junho": "06",
+        "Julho": "07",
+        "Agosto": "08",
+        "Setembro": "09",
+        "Outubro": "10",
+        "Novembro": "11",
+        "Dezembro": "12"
+    }
+
+    day, _, month, _, year, _, timestamp = date_str.split()
+
+    formatted_date = f"{day}/{months[month]}/{year} {timestamp}"
+    formatted_datetime = datetime.strptime(formatted_date, "%d/%m/%Y %H:%M")
+    return(formatted_datetime)
+
+# Checa se a data do ticket é menos de 10 minutos que a hora atual
+def should_send_ticket(ticket_date):
+    difference = datetime.now() - date_parser(ticket_date)
+    minutes = difference.total_seconds() / 60
+    if (minutes <= 10*(6*7)):
+        return(True)
+    else:
+        return(False)
---
+++ main.py
@@ -0,0 +1,85 @@
+import time
+import telebot
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+from date_utils import should_send_ticket
+
+# Carregando as variáveis de ambiente
+env = open('.env', 'r')
+token = env.readline().strip()
+username = env.readline().strip()
+password = env.readline().strip()
+
+bot = telebot.TeleBot(token)
+
+# /start
+@bot.message_handler(['start'])
+def start(msg:telebot.types.Message):
+    bot.reply_to(msg, "Bem Vindo. Você receberá os novos chamados abertos para a CTI/JC.")
+
+    while(True):
+        print("-----Iniciando Verificacao")
+        try:
+            # Instância do navegador
+            driver = webdriver.Chrome()
+            driver.get("https://suap.ifrn.edu.br/accounts/login/")
+
+            print("chegou aqui")
+
+            # Preenche os campos do login
+            driver.find_element(By.NAME, "username").send_keys(username)
+            driver.find_element(By.NAME, "password").send_keys(password)
+
+            time.sleep(1)
+
+            # Clica no botão de login
+            btn = driver.find_element(By.CLASS_NAME, "success")
+            btn.click()
+            time.sleep(5)
+
+            # Acessa a área de chamados
+            driver.get("https://suap.ifrn.edu.br/centralservicos/listar_chamados_suporte/")
+
+            # Obtendo o html da página
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # Obtendo os cards dos chamados
+            tickets_scrap = soup.find_all('div', class_='general-box')
+            print("Chamados Analisados: "+tickets_scrap.len())
+
+            # Separando os dados dos cards dos chamados
+            tickets_list = []
+            for ts in tickets_scrap:
+                ticket = []
+                ticket.append("https://suap.ifrn.edu.br"+ts.find('a').get('href'))
+                ticket.append(ts.find('strong').get_text())
+                ticket.append(ts.find('p').get_text())
+                
+                for ticket_data in ts.find_all('div', class_='list-item'):
+                    ticket.append(ticket_data.find('dd').get_text())
+
+                # Checagem da Data
+                if (should_send_ticket(ticket[4])):
+                    tickets_list.append({
+                        "link": ticket[0],
+                        "type": ticket[1],
+                        "description": ticket[2],
+                        "opener": ticket[3],
+                        "opening_date": ticket[4],
+                        "sla": ticket[5]
+                    })
+
+            driver.quit()
+
+            for ticket in tickets_list:
+                bot.send_message(msg.chat.id, "⚠️NOVO CHAMADO⚠️"+"\n\n- Tipo: "+ticket["type"]+".\n- Descrição: "+ticket["description"]+"\n\n- Interessado: "+ticket["opener"]+"\n- Data de Abertura: "+ticket["opening_date"]+"\n\n"+ticket["link"])
+            print("Chamados Enviados: "+tickets_list.len())
+
+            print("-----Verificação Concluída")
+        except:
+            print("-----Falha ao Realizar Verificacao")
+        time.sleep(600) # Nova verificação em 10 minutos
+
+bot.infinity_polling()
