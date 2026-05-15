d438dcf856ac91f66a2486d4142dc47c28664fe3
Breno Cardoso <elisiobreno@gmail.com>
2026-05-15 00:35:29 -0300

    0.2.0 - Option for number of tickets by attendant

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def start(msg:telebot.types.Message):
 
     while(True):
         print("-----Iniciando Verificacao")
+        driver = webdriver.Chrome()
         try:
             # Instância do navegador
-            driver = webdriver.Chrome()
             driver.get("https://suap.ifrn.edu.br/accounts/login/")
 
             # Preenche os campos do login
@@ -74,8 +74,6 @@ def start(msg:telebot.types.Message):
                         "sla": ticket[5]
                     })
 
-            driver.quit()
-
             for ticket in tickets_list:
                 bot.send_message(msg.chat.id, "⚠️NOVO CHAMADO⚠️"+"\n\n- Tipo: "+ticket["type"]+".\n- Descrição: "+ticket["description"]+"\n\n- Interessado: "+ticket["opener"]+"\n- Data de Abertura: "+ticket["opening_date"]+"\n\n"+ticket["link"])
 
@@ -88,6 +86,70 @@ def start(msg:telebot.types.Message):
             print("-----Verificação Concluída")
         except:
             print("-----Falha ao Realizar Verificacao")
+        finally:
+            driver.quit()
+
         time.sleep(600) # Nova verificação em 10 minutos
+
+# /ocupacao
+@bot.message_handler(['ocupacao'])
+def ocupacao(msg:telebot.types.Message):
+    message_await = bot.reply_to(msg, "Aguarde enquanto buscamos as informações...")
+
+    # Instância do navegador
+    driver = webdriver.Chrome()
+
+    try:
+        driver.get("https://suap.ifrn.edu.br/accounts/login/")
+
+        # Preenche os campos do login
+        driver.find_element(By.NAME, "username").send_keys(username)
+        driver.find_element(By.NAME, "password").send_keys(password)
+
+        time.sleep(1)
+
+        # Clica no botão de login
+        btn = driver.find_element(By.CLASS_NAME, "success")
+        btn.click()
+        time.sleep(4)
+
+        # Acessa a área de atendentes
+        driver.get("https://suap.ifrn.edu.br/centralservicos/relatorio_atendentes/?grupo_atendimento=30&uo=&atendentes_form=Aguarde...")
+
+        # Obtendo o html da página
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Obtendo os cards dos atendentes
+        people_scrap = soup.find_all('div', class_='card')
+
+        people_list = []
+        for p in people_scrap:
+            # Obtendo o nome
+            name_content = p.find('h4').get_text().split()
+            name = f"{name_content[0]} {name_content[-1]}"
+
+            # Obtendo o número de chamados
+            number_current_tickets = "0" # por default
+            number_content = p.find('a')
+            if (number_content != None):
+                number_content = number_content.get_text().split()
+                number_current_tickets = number_content[0]
+
+            people_list.append({
+                "name": name,
+                "number_of_tickets": number_current_tickets
+            })
+
+        # Montar a resposta parao chat
+        response = "Número de chamados em atendimento:"
+        for pl in people_list:
+            response = response+f"\n- {pl['name']}: {pl['number_of_tickets']}"
+        bot.send_message(msg.chat.id, response)
+    except:
+        print("Erro ao buscar informações.")
+    finally:
+        bot.delete_message(msg.chat.id, message_await.message_id)
+        driver.quit()
 
 bot.infinity_polling()
